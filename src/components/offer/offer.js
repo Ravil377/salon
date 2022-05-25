1c9deb43a177529d417883ee2f2294efa04ecc83
Ravil Shamilov <75186385+Ravil377@users.noreply.github.com>
2022-05-26 01:34:53 +0500

    push index

---
+++ src/components/offer/offer.js
@@ -0,0 +1,17 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 52.517, lng: 13.503 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
